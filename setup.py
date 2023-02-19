4e751d4583f147cffb97805a3f7fa3e7a67d7820
Tavaresiqueira <siqueiratav@gmail.com.br>
2023-02-19 17:31:13 -0300

    main V4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(name='quantitative_vale_model',
-      version='0.1',
+      version='0.2',
       description='Simple and profitable quantitative model to user in Brazilian VALE3 (VALE S.A.) Stocks',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
       url='https://github.com/Tavaresiqueira/quantitative_vale_model/',
-      download_url = 'https://github.com/Tavaresiqueira/quantitative_vale_model/archive/v0.1.tar.gz',
+      download_url = 'https://github.com/Tavaresiqueira/quantitative_vale_model/archive/v0.2.tar.gz',
       author='Joao Pedro Tavares',
       author_email='siqueiratav@gmail.com',
       packages=find_packages(),
